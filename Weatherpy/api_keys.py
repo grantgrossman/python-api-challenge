449ca4e6d67852bb8095a2be065558957b2be8c2
Grant Grossman <Pharaoh@Grants-MBP.home>
2020-07-17 13:06:55 -0400

    Homework API

--- Weatherpy/api_keys.py
+++ Weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cbc93d9c2fe831f467e8d8ce8b1572f8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCOSEhEOJ16IA862affawbWzwrEAJgrkt4"
